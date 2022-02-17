b9873a94656e68a28e9badea1f90f8f8cd24315f
yakovleva2904 <yakovlevaolga2904@gmail.com>
2022-02-17 20:20:37 +0200

    HW7 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createImgCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+            </div>`;
+        })
+        .join('');
+} 
+function onGalleryContainerClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    } 
+    const isUrlOfBigImage = e.target.dataset.source;
+    openImageInModal(isUrlOfBigImage);
+}
+
+function openImageInModal(url) {
+    const instance = basicLightbox.create(
+        `<img src="${url}">`
+    );
+    instance.show();
+
+    const visible = instance.visible();
+
+    if (visible) {
+    document.addEventListener("keydown", (e) => {
+      if (e.key !== "Escape") {
+        return;
+      }
+      instance.close();
+    });
+  }
+}
