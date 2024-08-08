032acfbcdffd0ca4d161003940065db6fdc35672
yakovleva2904 <yakovlevaolga2904@gmail.com>
2024-08-08 23:45:11 +0200

    task 5 is done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btn.addEventListener("click", (changeColor) => {
+  const color = getRandomHexColor();
+  spanColor.textContent = color;
+  document.body.style.background = color;
+ }
+)
+console.log(getRandomHexColor());
