b4eb62a58ff582ab865e3cc4850848b496157dd7
yakovleva2904 <yakovlevaolga2904@gmail.com>
2024-08-08 23:16:13 +0200

    task 4 is done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = {
+        email: formEl.elements.email.value.trim(),
+        password: formEl.elements.password.value.trim(),
+    }
+    if (formData.email === "" || formData.password === "") {
+		return alert("All form fields must be filled in");
+	}
+
+    formEl.reset();
+    console.log(formData);
+};
+
+formEl.addEventListener('submit', onFormSubmit);
