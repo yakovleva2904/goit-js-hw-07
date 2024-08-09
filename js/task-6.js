942fa4453822154ca8a615111f27add49c7bd0d2
yakovleva2904 <yakovlevaolga2904@gmail.com>
2024-08-09 22:16:47 +0200

    task 6 is done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+destroyBtn.addEventListener("click", destroyHandler);
+createBtn.addEventListener("click", createHandler);
+
+function createBoxes(amount) {
+	amount = Number(amount);
+	destroyHandler();
+	let size = 30;
+	const arr = [];
+	for (let i = 0; i < amount; i++) {
+		arr.push(
+			`<div style="height:${size}px;width:${size}px;background-color:${getRandomHexColor()}"></div>`,
+		);
+		size += 10;
+	}
+	boxes.insertAdjacentHTML("beforeend", arr.join(""));
+	input.value = null;
+}
+
+function destroyHandler() {
+	boxes.innerHTML = "";
+}
+
+function createHandler() {
+	if (input.value > 0 && input.value <= 100) {
+		createBoxes(input.value);
+	} else {
+		alert(
+			`Input value can not be ${input.value}. Set a number between 1 and 100`,
+		);
+	}
+}
